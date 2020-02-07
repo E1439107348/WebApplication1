655694478b0382bc1e06994e0f5ea861165e0f45
洋 邱 <1439107348@qq.com>
2020-02-07 12:24:46 +0800

    添加mysql链接查询数据库

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using ICSharpCode.SharpZipLib.Zip;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -13,10 +15,14 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+
+
+
         public ActionResult Index()
         {
 
-           
+            readSql();
+            return View();
             //string TorepotFiles = @"G:\syxWork\工作使用到的文件等等\2020-1-16任务\xml导入导出\按机构导出文件_C59.F19.P11_浙江省人民政府20200119100455.zip";
             //string reportPath = @"G:\syxWork\工作使用到的文件等等\2020-1-16任务\xml导入导出\备份\新建文件夹";
             //UnzipTheFiles(TorepotFiles, reportPath);
@@ -25,17 +31,12 @@ namespace WebApplication1.Controllers
 
                 try
                 {
-
-
-
                     //string SQLStr = string.Format("insert into   UserTS VALUES('{0}', '{1}')", Guid.NewGuid().ToString(), DateTime.Now.ToString());
-
 
                     string SQLStr = string.Format("INSERT INTO code_value(CODE_VALUE_SEQ, CODE_TYPE, CODE_COLUMN_NAME, CODE_LEVEL, CODE_VALUE, ININO, SUB_CODE_VALUE, CODE_NAME, CODE_NAME2, CODE_NAME3, CODE_REMARK, CODE_SPELLING, ISCUSTOMIZE, CODE_ASSIST, CODE_STATUS, CODE_LEAF, START_DATE, STOP_DATE) VALUES (9999, 'ZB79', NULL, NULL, '1', NULL, '-1', '领导班子换届考察1', '1', '领导班子换届考察1', NULL, 'LDBZHJKC', '0', NULL, '1', '1', NULL, NULL)");
                     db.Database.ExecuteSqlCommand(SQLStr);
                 }
                 catch (Exception ex)
-
                 {
                     var ts = ex.ToString();
                     throw;
@@ -48,75 +49,38 @@ namespace WebApplication1.Controllers
 
         #region 修改中内容
 
+        //创建command对象      
+        private MySqlCommand cmd = null;
+        //创建connection连接对象  
+        private MySqlConnection conn = null;
+        //数据库连接字符串  
+        String connstr = "server=localhost;port=45017;uid=root;pwd=admin;database=zwhzyq";
+        //建立数据库连接  
+
+        public void readSql()
+        {
+            conn = new MySqlConnection(connstr);
+            //需要读取的字段
+            List<string> ReadField = new List<string>();
+
+            //从数据库读取字段
+            MySqlDataReader reader = null;
+            string SQLStr = string.Format("SELECT CODE_TYPE,CODE_NAME,CODE_VALUE,CODE_NAME2,CODE_NAME3 FROM code_value WHERE CODE_TYPE IN ('ZB79','ZB75')");
+
+            var tt = realsql.QueryExecute(SQLStr);
+            var ts = "";
+
+        }
+
+
 
         #endregion
 
 
 
-        /// <summary>
-        /// 解压文件 1.01
-        /// </summary>
-        ///<param name = "ZipPath" > 需要被解压的文件 </ param >
-        /// <param name="Path">解压后文件的路径</param>
-        public string UnzipTheFiles(string TorepotFiles, string reportPath)
-        {
-
-            ZipInputStream s = new ZipInputStream(System.IO.File.OpenRead(TorepotFiles));
-
-            ZipEntry theEntry;
-            try
-            {
-                while ((theEntry = s.GetNextEntry()) != null)
-                {
-                    string fileName = System.IO.Path.GetFileName(theEntry.Name);
-                    //生成解压目录
-                    Directory.CreateDirectory(reportPath);
-
-                    if (fileName != String.Empty)
-                    {
-                        //解压文件
-                        FileStream streamWriter = System.IO.File.Create(reportPath + @"\测试路径无" + fileName);
 
 
-                        int size = 2048;
-                        byte[] data = new byte[2048];
-                        while (true)
-                        {
-                            size = s.Read(data, 0, data.Length);
-                            if (size > 0)
-                            {
-                                streamWriter.Write(data, 0, size);
-                            }
-                            else
-                            {
-
-                                streamWriter.Close();
-                                streamWriter.Dispose();
-                                break;
-                            }
-                        }
-
-                        streamWriter.Close();
-                        streamWriter.Dispose();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
-            finally
-            {
-                s.Close();
-                s.Dispose();
-            }
-
-            return reportPath;
-        }
-
-
-        #region 读写xml
+        #region 1.读写xml
 
         /// <summary>
         ///1.1 读取xml文件 1.0
@@ -265,13 +229,13 @@ namespace WebApplication1.Controllers
             xmlDocument.Save(@"K:\write3.xml");
 
 
-//输出格式
-   //         <? xml version = "1.0" encoding = "UTF-8" ?>
-   //< root >
-   //  < items name = "01" >
-   //    < item key = "key1" value = "value" />
-   //     </ items >
-   //   </ root >
+            //输出格式
+            //         <? xml version = "1.0" encoding = "UTF-8" ?>
+            //< root >
+            //  < items name = "01" >
+            //    < item key = "key1" value = "value" />
+            //     </ items >
+            //   </ root >
 
         }
         #endregion
@@ -279,7 +243,70 @@ namespace WebApplication1.Controllers
 
 
 
+        #region 2.对压缩文件操作
+        /// <summary>
+        /// 解压文件 1.01
+        /// </summary>
+        ///<param name = "ZipPath" > 需要被解压的文件 </ param >
+        /// <param name="Path">解压后文件的路径</param>
+        public string UnzipTheFiles(string TorepotFiles, string reportPath)
+        {
 
+            ZipInputStream s = new ZipInputStream(System.IO.File.OpenRead(TorepotFiles));
+
+            ZipEntry theEntry;
+            try
+            {
+                while ((theEntry = s.GetNextEntry()) != null)
+                {
+                    string fileName = System.IO.Path.GetFileName(theEntry.Name);
+                    //生成解压目录
+                    Directory.CreateDirectory(reportPath);
+
+                    if (fileName != String.Empty)
+                    {
+                        //解压文件
+                        FileStream streamWriter = System.IO.File.Create(reportPath + @"\测试路径无" + fileName);
+
+
+                        int size = 2048;
+                        byte[] data = new byte[2048];
+                        while (true)
+                        {
+                            size = s.Read(data, 0, data.Length);
+                            if (size > 0)
+                            {
+                                streamWriter.Write(data, 0, size);
+                            }
+                            else
+                            {
+
+                                streamWriter.Close();
+                                streamWriter.Dispose();
+                                break;
+                            }
+                        }
+
+                        streamWriter.Close();
+                        streamWriter.Dispose();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                s.Close();
+                s.Dispose();
+            }
+
+            return reportPath;
+        }
+
+        #endregion
 
 
 
---
+++ WebApplication1/Models/realsql.cs
@@ -0,0 +1,49 @@
+﻿
+
+using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WebApplication1.Models
+{
+    public class realsql
+    {
+
+        public static IList<code_value> QueryExecute(string sql)
+        {
+             
+            //var daset = DbHelperMySQL.Query(strSql.ToString());
+            using (IDbConnection connection = OpenConnection())
+            {
+                // 查询
+                var list = connection.Query<code_value>(sql).ToList<code_value>();
+              
+
+                return list;
+            }
+        }
+
+        /// <summary>
+        /// 打开连接
+        /// </summary>
+        /// <returns></returns>
+        public static IDbConnection OpenConnection()
+        {
+            //IDbConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["AdventureWorks"].ConnectionString);
+
+
+            IDbConnection connection = new MySqlConnection(connectionString1);
+            connection.Open();
+
+            return connection;
+        }
+        private static readonly string connectionString1 = System.Configuration.ConfigurationManager.ConnectionStrings["conncodefirst"].ToString();
+
+      
+    }
+}
